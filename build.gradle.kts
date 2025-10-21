9a9443c674946e58015fcb1fd165dcd785076e73
Matheus Feitosa <82462744+Chambs@users.noreply.github.com>
2025-10-21 16:16:59 -0300

    Remove package attribute from main manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
     // 👇 OBRIGATÓRIO com Kotlin 2.0 + Compose
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
